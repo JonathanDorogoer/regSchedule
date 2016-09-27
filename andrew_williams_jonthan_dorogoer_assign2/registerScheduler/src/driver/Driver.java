2a927d73988ecee811f3225e3849954384ef73f9
Andrew Williams <awilli64@binghamton.edu>
2016-09-27 19:32:45 -0400

    algo skeleton

--- andrew_williams_jonthan_dorogoer_assign2/registerScheduler/src/driver/Driver.java
+++ andrew_williams_jonthan_dorogoer_assign2/registerScheduler/src/driver/Driver.java
@@ -2,39 +2,44 @@ import java.util.ArrayList;
 
 public class Driver {
     public static void main(String[] args) {
-		FileProcessor fp = new FileProcessor("data.txt");
+	final int NUM_CLASSES = 7;
+	
+      	FileProcessor fp = new FileProcessor("data.txt");
 	ArrayList<Student> studentList = new ArrayList<Student>();
 	ObjectPool classPool = ObjectPool.getInstance();
 
+	int s = 0;
+
 	while (fp.hasNextLine()) {
+	    ++s;
 	    Student curStudent = new Student(fp.getNextLine());
-	    
-	    int curClassIndex = 0;
-
-	    while (curStudent.numClasses < 5) {
-		while (curStudent.numClasses < 5 && curClassIndex < 7) {
-		    curClassIndex = curStudent.nextDesiredClass(curClassIndex);
-		    
-
-		}
-
-
-		//IF WE HAVE TO KICK SOMEONE OUT
 		
+	    //try to enroll Student in five classes
+	    int curPref = -1;
+	    int curClass;
+	    while (curStudent.numClasses() < 5) {
+		curClass = curStudent.nextDesiredClass(curPref);
+		if (curClass == -1) break;
+		if (classPool.getSeat(curClass)) {
+		    curStudent.enrollInClass(curClass);
+		    System.out.printf("Enrolling student_%d in class %d.\n", s, curClass);
+		}
+		curPref = curStudent.getPrefByClass(curClass);
 	    }
-
-
+	    
+	    //If we were unable to assign five classes, we need to kick someone out of a full class
+	    curClass = curStudent.nextDesiredClass(-1);
+	    int openClass;
+	    for (int i = 0; i < NUM_CLASSES; ++i) {
+		if (!classPool.isFull(i)) {
+		    openClass = i;
+		    break;
+		}
+	    }
+	    for (Student studentToKick : studentList) {
+	    }
+	    studentList.add(curStudent);
 	}
-	/*
-
-	Student S = new Student ("Student_x 1 5 2 3 7 4 6");
 	
-	S.enrollInClass(3);
-	S.enrollInClass(6);
-	for (int i = 0; i < 7; ++i) {
-	    System.out.printf("%d ", S.nextDesiredClass(i));
-	}
-	System.out.printf("\n");
-	*/
     }
 }
--- andrew_williams_jonthan_dorogoer_assign2/registerScheduler/src/driver/Student.java
+++ andrew_williams_jonthan_dorogoer_assign2/registerScheduler/src/driver/Student.java
@@ -3,33 +3,35 @@ import java.util.ArrayList;
 public class Student {
     private final int NUM_CLASSES = 7;
 
-    private int[] prefs                 = new int[NUM_CLASSES];
+    private int[]     classNumByPref    = new int[NUM_CLASSES];
+    private int[]     prefByClassNum    = new int[NUM_CLASSES];
     private boolean[] classesEnrolledIn = new boolean[NUM_CLASSES];
 
-    private int[] classPrefs           = new int[NUM_CLASSES];
-
-    /*
-    public Student(int pref0, int pref1, int pref2, int pref3, int pref4, int pref5, int pref6) {
-	prefs[0] = pref0;
-	prefs[1] = pref1;
-	prefs[2] = pref2;
-	prefs[3] = pref3;
-	prefs[4] = pref4;
-	prefs[5] = pref5;
-	prefs[6] = pref6;
-	
-	for (int i = 0; i < NUM_CLASSES; ++i)
-	    classesEnrolledIn[i] = false;
-
-
-	 
-	
-    }
-    */
 
     public Student (String inputLine) {
 	String temp[] = inputLine.split("\\s+");
 	
+	for (int i = 0; i < NUM_CLASSES; ++i) {
+	    classesEnrolledIn[i] = false;
+	    prefByClassNum[i] = Integer.parseInt(temp[i + 1]) - 1;
+	}
+	    
+	for (int i = 0; i < NUM_CLASSES; ++i) {
+	    classNumByPref[prefByClassNum[i]] = i;
+	}
+
+	System.out.printf("classNumByPref: ");
+	for (int i = 0; i < NUM_CLASSES; ++i)
+	    System.out.printf("%d ", classNumByPref[i]);
+	System.out.printf("\n");
+
+	System.out.printf("prefByClassNum: ");
+	for (int i = 0; i < NUM_CLASSES; ++i)
+	    System.out.printf("%d ", prefByClassNum[i]);
+	System.out.printf("\n");
+
+
+	/*
 	for (int i = 1; i <= 7; ++i)
 	    prefs[i - 1] = Integer.parseInt(temp[i]);
 
@@ -39,20 +41,20 @@ public class Student {
 	for (int i = 1; i <= NUM_CLASSES; ++i) {
 	    classPrefs[Integer.parseInt(temp[i]) - 1] = i;
 	}
-
-
+	*/
+	/*
 	System.out.printf("prefs: %d %d %d %d %d %d %d\n", prefs[0], prefs[1], prefs[2], prefs[3], prefs[4], prefs[5], prefs[6]);
-	System.out.printf("classPrefs: %d %d %d %d %d %d %d\n", classPrefs[0], classPrefs[1], classPrefs[2], classPrefs[3], classPrefs[4], classPrefs[5], classPrefs[6]);
+	System.out.printf("classPrefs: %d %d %d %d %d %d %d\n", classPrefs[0], classPrefs[1], classPrefs[2], classPrefs[3], classPrefs[4], classPrefs[5], classPrefs[6]);*/
     }
 
     public void dropClass (int classNum) {
-	assert (classNum <= NUM_CLASSES);
-	classesEnrolledIn[classNum - 1] = false;
+	assert (classNum < NUM_CLASSES);
+	classesEnrolledIn[classNum ] = false;
     }
 
     public void enrollInClass (int classNum) {
-	assert (classNum <= NUM_CLASSES);
-	classesEnrolledIn[classNum - 1] = true;
+	assert (classNum < NUM_CLASSES);
+	classesEnrolledIn[classNum] = true;
     }
 
     public String getClasses() {
@@ -60,7 +62,7 @@ public class Student {
 
 	for (int i = 0; i < NUM_CLASSES; ++i) {
 	    if (classesEnrolledIn[i] == true)
-		retVal = retVal + (i + 1) + ' ';
+		retVal = retVal + i + ' ';
 	}
 
 	return retVal;
@@ -76,14 +78,31 @@ public class Student {
 	return retVal;
     }
 
-    public int nextDesiredClass(int curDesiredClass) {
-	while (curDesiredClass < NUM_CLASSES) {
+    public int getPrefByClass (int classNum) {
+	return prefByClassNum[classNum];
+    }
+
+
+    //takes in a preference score, and returns the class number of the class with the next highest
+    //preference score that student in not enrolled in
+    //if student is already enrolled in all classes more desirable than inPrefScore, -1 is returned
+    public int nextDesiredClass(int inPrefScore) {
+	int curPrefScore = inPrefScore + 1;
+	while (curPrefScore < NUM_CLASSES) {
+	    if ( !classesEnrolledIn[classNumByPref[curPrefScore]])
+		return classNumByPref[curPrefScore];
+	    ++curPrefScore;
+	}
+
+	return -1;
+	/*while (curDesiredClass < NUM_CLASSES) {
 	    if (!classesEnrolledIn[classPrefs[curDesiredClass] - 1])
 		return classPrefs[curDesiredClass]; 
 	    ++curDesiredClass;
 	}
 	assert (false);
 	return -1;
+	*/
     }
     /*
     //returns actual class number, not array index
